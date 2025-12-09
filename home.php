f2ccd4febb4693911b3f88a38dbfe8704abc3319
MFauzan125 <mf4138633@gmail.com>
2025-12-09 20:55:39 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php require('header.php'); ?> 
+  
+<div class="content"> 
+   <h2>Ini Halaman Home</h2> 
+   <footer> 
+           <p>&copy; 2021, Informatika, Universitas Pelita Bangsa</p> 
+       </footer> 
+   </div> 
+</body> 
+</html>
